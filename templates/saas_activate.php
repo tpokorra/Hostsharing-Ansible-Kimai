3c04557f83e239957787e156ecee07f57ed30b8d
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-09-28 06:38:22 +0200

    support saas operation

---
+++ templates/saas_activate.php
@@ -0,0 +1,33 @@
+<?php
+
+function Get($index, $defaultValue) {
+  return isset($_GET[$index]) ? $_GET[$index] : $defaultValue;
+}
+
+# check SaasActivationPassword
+if (Get('SaasActivationPassword', 'invalid') != '{{SaasActivationPassword}}') {
+  echo '{"success": false, "msg": "invalid SaasActivationPassword"}';
+  exit(1);
+}
+
+try {
+  $USER_EMAIL_ADDRESS = Get('UserEmailAddress', '');
+  if (empty($USER_EMAIL_ADDRESS)) {
+    echo '{"success": false, "msg": "missing email address"}';
+    exit(1);
+  }
+
+  $pdo = new PDO('mysql:host=localhost;dbname={{pac}}_{{user}}', '{{pac}}_{{user}}', '{{password}}');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "update kimai2_users set email=:email, registration_date=NOW() where id=1";
+  $statement = $pdo->prepare($sql);
+  $statement->execute(array(':email' => $USER_EMAIL_ADDRESS));
+}
+catch (Exception $e) {
+    // echo 'Exception caught: ',  $e->getMessage(), "\n";
+    echo '{"success": false, "msg": "error happened"}';
+    exit(1);
+}
+
+echo '{"success": true}';
+?>
